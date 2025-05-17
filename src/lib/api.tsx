802a2770e2763ba9cab3e793ae99d76bf7f0cfc8
Kelly Kingston <okellykings220@gmail.com>
2025-05-17 05:10:26 +0100

    betpool

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const createApiClient = (authToken: string | null) => {
   return axios.create({
-    baseURL: "https://betpool.com/api/drives",
+    baseURL: "https://betpool.online/api/drives",
     withCredentials: true,
     headers: {
       "Content-Type": "application/json",
